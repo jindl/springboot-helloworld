d04b85e7d96b1fd8dccbae11a5a428abce65de43
davisjin <jindl@tom.com>
2017-07-05 22:56:44 +0800

    增加 tomcat 的支持

--- src/main/java/org/spring/springboot/Application.java
+++ src/main/java/org/spring/springboot/Application.java
@@ -2,6 +2,8 @@ package org.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Spring Boot 应用启动类
@@ -10,11 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 // Spring Boot 应用的标识
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        // 程序启动入口
-        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        SpringApplication.run(Application.class,args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
